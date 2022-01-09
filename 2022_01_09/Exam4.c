c4ddecf77112c1868dc1d2772b81750e43a359b3
yes25 <83991028+yes25cho@users.noreply.github.com>
2022-01-10 00:03:41 +0900

    '%d'를 이용한 너비 조정 연습(배열버전)

--- 2022_01_09/Exam4.c
+++ 2022_01_09/Exam4.c
@@ -1,9 +1,29 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i1, i2, i3;
+//	scanf("%d %d %d", i1, i2, i3);
+//	printf("%10d%10d%10d", i1, i2, i3);
+//	prinf();
+//}
+
+// 배열로 연습
 #include <stdio.h>
 
 int main()
 {
-	int i1, i2, i3;
-	scanf("%d %d %d", i1, i2, i3);
-	printf("%10d%10d%10d", i1, i2, i3);
-	prinf();
-}
+	int num[3];
+	int i;int j;
+	//입력 
+	for(i = 0; i<3; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+	//출력 
+	for(j =0; j<3; j++)
+	{
+		printf("%10d", num[j]);
+	}
+	return 0;	
+} 
